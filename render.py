10f9eddad8a6c9bfe423cb8b899669e9052bb5f4
tyuijhgs <Mr.Christopher.Blackman@gmail.com>
2016-08-03 22:24:50 -0400

    its a secret

--- render.py
+++ render.py
@@ -4,7 +4,9 @@ import os
 def main():
 	os.system('echo "markdown templates/md/resume/resume.md > templates/html/resume/body.html" ')
 	os.system('markdown templates/md/resume/resume.md > templates/html/resume/body.html')
-	
+	os.system('echo "markdown templates/md/manga/manga.md > templates/html/manga/body.html" ')
+	os.system('markdown templates/md/manga/manga.md > templates/html/manga/body.html')
+		
 	base_path = 'templates/html/'
 	base_output_path = 'output/'
 	block = 'body'
@@ -12,8 +14,10 @@ def main():
 	home = Exagogi(base_path + 'home/index.html', base_output_path + 'index.html', base_path + 'home/body.html', block)
 	projects = Exagogi(base_path + 'projects/projects.html', base_output_path + 'projects.html', base_path + 'projects/body.html', block)
 	resume = Exagogi(base_path + 'resume/resume.html', base_output_path + 'resume.html', base_path + 'resume/body.html',block)
+	manga = Exagogi(base_path + 'manga/manga.html', base_output_path + 'manga.html', base_path + 'manga/body.html',block)
 	about.render()
 	home.render()
 	projects.render()
 	resume.render()
+	manga.render()
 main()
